cb29bb820adee3a812dd36159eab378447b6144a
Jen <lunchbag@gmail.com>
2019-09-25 14:26:50 -0400

    Support res.cookie

--- src/interfaces/controller.ts
+++ src/interfaces/controller.ts
@@ -1,25 +1,33 @@
-
 export class Controller {
-    private statusCode?: number = undefined;
-    private headers = {} as { [name: string]: string | undefined };
+  private statusCode?: number = undefined;
+  private headers = {} as { [name: string]: string | undefined };
+  private cookies = [] as { name: string; value: string; options?: any }[];
 
-    public setStatus(statusCode: number) {
-        this.statusCode = statusCode;
-    }
+  public setStatus(statusCode: number) {
+    this.statusCode = statusCode;
+  }
 
-    public getStatus() {
-        return this.statusCode;
-    }
+  public getStatus() {
+    return this.statusCode;
+  }
 
-    public setHeader(name: string, value?: string) {
-        this.headers[name] = value;
-    }
+  public setHeader(name: string, value?: string) {
+    this.headers[name] = value;
+  }
 
-    public getHeader(name: string) {
-        return this.headers[name];
-    }
+  public getHeader(name: string) {
+    return this.headers[name];
+  }
 
-    public getHeaders() {
-        return this.headers;
-    }
+  public getHeaders() {
+    return this.headers;
+  }
+
+  public setCookie(name: string, value: string, options?: any) {
+    this.cookies.push({ name, value, options });
+  }
+
+  public getCookies(): { name: string; value: string; options?: any }[] {
+    return this.cookies;
+  }
 }
